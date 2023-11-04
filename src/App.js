00b9a1b9809ab1c21d8b903bc996d6afe78c93bc
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-11-04 10:33:31 +0300

    complete assignment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+function NavBar (){
+  return <h1>NavBar tools</h1>
+}
+
+function Home (){
+  return <h2>Home tools</h2>
+}
+
 function App() {
   return (
     <div>
+      <Home/>
+      <NavBar/>
       <Article />
       <Comment />
     </div>
